833b9a8feaad6da15b8ade41eda439271f9dbead
Jeff Button <jeffreyjbutton@gmail.com>
2019-01-12 20:48:04 -0800

    convert classes to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
-import BurgerBuilder from './containers/burger-builder/burger-builder';
+import React from 'react';
 import Layout from './components/layout/layout';
+import BurgerBuilder from './containers/burger-builder/burger-builder';
 
-class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <Layout>
+      <BurgerBuilder />
+    </Layout>
+  );
+};
--- src/containers/burger-builder/burger-builder.tsx
+++ src/containers/burger-builder/burger-builder.tsx
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Burger from '../../components/burger/burger';
 const BuildControls = styled.div``;
 
-class BurgerBuilder extends Component {
-  render() {
-    return (
-      <>
-        <Burger>Burger</Burger>
-        <BuildControls>Build controls</BuildControls>
-      </>
-    );
-  }
-}
-
-export default BurgerBuilder;
+export default () => {
+  return (
+    <>
+      <Burger>Burger</Burger>
+      <BuildControls>Build controls</BuildControls>
+    </>
+  );
+};
